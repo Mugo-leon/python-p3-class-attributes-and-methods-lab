ad2ed2d2a3083f814af603ef9f776ff295979f70
Leon <leon.muriuki@student.moringaschool.com>
2024-01-26 22:24:18 +0300

    Completed lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,57 @@
 class Song:
-    pass
+    # Class attributes
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        # Instance attributes
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Increment the count of songs
+        Song.add_song_to_count()
+
+        # Add genre and artist to class attributes
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+
+        # Update genre count and artist count
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        # Increment the count of songs
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        # Add new genre to the list if not already present
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        # Add new artist to the list if not already present
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        # Add to genre_count dictionary
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        # Add to artist_count dictionary
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
